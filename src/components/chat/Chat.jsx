c76ace94a44e85e4830fc645aa192cf62428371c
Saeed Haddad <haddadsaeed6@gmail.com>
2024-08-27 15:26:23 +0300

    Detail Section 1.0

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./chat.css";
 import EmojiPicker from "emoji-picker-react";
 const Chat = () => {
--- src/components/detail/Detail.jsx
+++ src/components/detail/Detail.jsx
@@ -1,5 +1,83 @@
 import "./detail.css";
 const Detail = () => {
-  return <div className="detail">Detail</div>;
+  return (
+    <div className="detail">
+      <div className="user">
+        <img src="/avatar.png" alt="" />
+        <h2>Jane Doe</h2>
+        <p>Lorem Ipsum Dolor Set Amit</p>
+      </div>
+      <div className="info">
+        <div className="option">
+          <div className="title">
+            <span>Chat Settings</span>
+            <img src="/arrowUp.png" alt="" />
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Privacy & Help</span>
+            <img src="/arrowUp.png" alt="" />
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Shared Photos</span>
+            <img src="/arrowDown.png" alt="" />
+          </div>
+          <div className="photos">
+            <div className="photoItem">
+              <div className="photoDetail">
+                <img
+                  src="https://images.pexels.com/photos/27438918/pexels-photo-27438918/free-photo-of-a-beach-with-rocks-and-sand-at-sunset.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                  alt=""
+                />
+                <span>photo_2024_2.png</span>
+              </div>
+              <img src="/download.png" alt="" className="icon" />
+            </div>
+            <div className="photoItem">
+              <div className="photoDetail">
+                <img
+                  src="https://images.pexels.com/photos/27438918/pexels-photo-27438918/free-photo-of-a-beach-with-rocks-and-sand-at-sunset.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                  alt=""
+                />
+                <span>photo_2024_2.png</span>
+              </div>
+              <img src="/download.png" alt="" className="icon" />
+            </div>
+            <div className="photoItem">
+              <div className="photoDetail">
+                <img
+                  src="https://images.pexels.com/photos/27438918/pexels-photo-27438918/free-photo-of-a-beach-with-rocks-and-sand-at-sunset.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                  alt=""
+                />
+                <span>photo_2024_2.png</span>
+              </div>
+              <img src="/download.png" alt="" className="icon" />
+            </div>
+            <div className="photoItem">
+              <div className="photoDetail">
+                <img
+                  src="https://images.pexels.com/photos/27438918/pexels-photo-27438918/free-photo-of-a-beach-with-rocks-and-sand-at-sunset.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                  alt=""
+                />
+                <span>photo_2024_2.png</span>
+              </div>
+              <img src="/download.png" alt="" className="icon" />
+            </div>
+          </div>
+        </div>
+
+        <div className="option">
+          <div className="title">
+            <span>Shared Files</span>
+            <img src="/arrowUp.png" alt="" />
+          </div>
+        </div>
+        <button>Block User</button>
+      </div>
+    </div>
+  );
 };
 export default Detail;
